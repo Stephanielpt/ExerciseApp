44d3d99536aeebd5155dda47c89700d301a33875
Stephanielpt <stephanielampotang@gmail.com>
2018-02-25 03:30:40 -0800

    Initial Commit

---
+++ exercite!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  exercite!
+//
+//  Created by Stephanie Lampotang on 2/25/18.
+//  Copyright © 2018 Stephanie Lampotang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
